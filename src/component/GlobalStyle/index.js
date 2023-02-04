c048fa2e54d331f18023ad433e51cf28248486c2
Nguyen Thanh Tuan <ngthtuan333@gmail.com>
2023-02-04 11:39:52 +0700

    Giải quyết vấn đề nhiều file index.js

--- src/component/GlobalStyle/index.js
+++ src/component/GlobalStyle/index.js
@@ -1,12 +1,1 @@
-import PropTypes from 'prop-types';
-import './GlobalStyle.scss';
-
-function GlobalStyle({ children }) {
-    return children;
-}
-
-GlobalStyle.propTypes = {
-    children: PropTypes.node.isRequired,
-};
-
-export default GlobalStyle;
+export { default } from './GlobalStyle';
